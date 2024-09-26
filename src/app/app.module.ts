b68b6a23a71936207b50df1a3eb772c4bf245af6
dariojorge <jorge12dario12@hotmail.com>
2024-09-26 11:57:12 +0100

    updating the app.modules.ts to include the header and footer comps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    HeaderComponent,
+    FooterComponent
+],
   providers: [],
   bootstrap: [AppComponent]
 })
