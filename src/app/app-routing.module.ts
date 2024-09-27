e4ffcce0a8f0894bd4ee38c23adbfd244507dff7
dariojorge <jorge12dario12@hotmail.com>
2024-09-27 18:08:49 +0100

    Attempting to fix routing 08

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,13 +9,13 @@ import { Project1Component } from './projects/project1/project1.component';
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: '/home', component: HomeComponent },
-    { path: '#/resume', component: ResumeComponent },
-    { path: '#/about', component: AboutComponent },
+    { path: '/resume', component: ResumeComponent },
+    { path: '/about', component: AboutComponent },
     { path: '/project/project1', component: Project1Component }
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, { useHash: true })],
+    imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
 })
 export class AppRoutingModule { }
